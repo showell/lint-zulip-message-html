a652386cb776808bf152769c76635681fdbe43f4
Steve Howell <showell30@yahoo.com>
2024-12-26 23:11:03 +0000

    Extract rules.py

--- foo.py
+++ foo.py
@@ -1,46 +1,12 @@
 from lxml import html
+from rules import RULES
 import json
-
-# Sample HTML string
-html_string = """
-"""
 
 def check_rule(rule, node):
     return rule["tag"] == node.tag
 
 def validate(node):
-    print(node.tag)
-    rules = [
-        dict(tag="a"),
-        dict(tag="blockquote"),
-        dict(tag="br"),
-        dict(tag="code"),
-        dict(tag="del"),
-        dict(tag="div"),
-        dict(tag="em"),
-        dict(tag="h1"),
-        dict(tag="h6"),
-        dict(tag="img"),
-        dict(tag="li"),
-        dict(tag="ol"),
-        dict(tag="math"),
-        dict(tag="mi"),
-        dict(tag="mn"),
-        dict(tag="mo"),
-        dict(tag="mrow"),
-        dict(tag="msub"),
-        dict(tag="msup"),
-        dict(tag="mtext"),
-        dict(tag="p"),
-        dict(tag="pre"),
-        dict(tag="semantics"),
-        dict(tag="span"),
-        dict(tag="strong"),
-        dict(tag="time"),
-        dict(tag="ul"),
-        dict(tag="video"),
-    ]
-    for rule in rules:
+    for rule in RULES:
         if check_rule(rule, node):
             break
     else:
