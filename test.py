64ec0e02d13173743b3f7a5f3e7237a84a2b3c26
Steve Howell <showell30@yahoo.com>
2024-12-27 11:02:55 +0000

    extract validator

--- test.py
+++ test.py
@@ -1,33 +1,6 @@
 import json
-
 from lxml import html
-from rules import RULES
-
-def validate_children(rule, node):
-    children = node.getchildren()
-    if children and not rule.get("children"):
-        print("UNEXPECTED CHILDREN", node.tag, node.text)
-        return False
-
-    for c in children:
-        if not validate(c):
-            return False
-    return True
-
-def validate_rule(rule, node):
-    if not validate_children(rule, node):
-        return False
-    return True
-
-def validate(node):
-    for rule in RULES:
-        if rule["tag"] == node.tag:
-            if validate_rule(rule, node):
-                return True
-
-    print("UNKNOWN OR BROKEN TAG", node.tag, node.text)
-    return False
-
+from validator import validate
 
 json_payload = open("cases.json").read()
 for fixture in json.loads(json_payload)['regular_tests']:
