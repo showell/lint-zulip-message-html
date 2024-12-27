35f311e7815c8bfe7a7e1b8d836ab154e2bf0b25
Steve Howell <showell30@yahoo.com>
2024-12-27 16:11:23 +0000

    text checks

--- rules.py
+++ rules.py
@@ -70,3 +70,28 @@ LEAF_TAGS = {
     "time",
     "video",
 }
+
+TEXT_FRIENDLY_NODES = {
+    "a",
+    "annotation",
+    "code",
+    "del",
+    "em",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "li",
+    "mi",
+    "mn",
+    "mo",
+    "mtext",
+    "p",
+    "span",
+    "strong",
+    "td",
+    "th",
+    "time",
+}
--- validator.py
+++ validator.py
@@ -1,27 +1,42 @@
-from rules import ALL_TAGS, RESTRICTED_TAGS, LEAF_TAGS
+from rules import ALL_TAGS, RESTRICTED_TAGS, LEAF_TAGS, TEXT_FRIENDLY_NODES
 from lxml import etree
-
 
 class BrokenException(Exception):
     pass
 
 
-def node_text(node):
+def full_node_text(node):
     return etree.tostring(node, encoding="unicode", method="html")
+
+
+def has_raw_text(node):
+    # Check if the node itself has text content
+    if node.text and node.text.strip():
+        return True
+
+    # Check if any child node has tail text
+    for child in node.iterchildren():
+        if child.tail and child.tail.strip():
+            return True
+
+    # If no text was found, return False
+    return False
 
 
 def validate_children(node):
     children = node.getchildren()
 
     if children and node.tag in LEAF_TAGS:
-        print("UNEXPECTED CHILDREN", node.tag, node_text(node))
+        print("UNEXPECTED CHILDREN", node.tag, full_node_text(node))
         raise BrokenException
 
     for c in children:
         if node.tag in RESTRICTED_TAGS:
             if c.tag not in RESTRICTED_TAGS[node.tag]:
                 print(
-                    f"UNEXPECTED CHILD {c.tag} OF {node.tag}", node.tag, node_text(node)
+                    f"UNEXPECTED CHILD {c.tag} OF {node.tag}",
+                    node.tag,
+                    full_node_text(node),
                 )
                 raise BrokenException
 
@@ -31,12 +46,17 @@ def validate_children(node):
 
 
 def validate(node):
+    if has_raw_text(node) and node.tag not in TEXT_FRIENDLY_NODES:
+        print(f"TAG {node.tag} unexpectedly has text")
+        print(full_node_text(node))
+        raise BrokenException
+
     if node.tag not in ALL_TAGS:
-        print("UNSUPPORTED TAG", node.tag, node_text(node))
+        print("UNSUPPORTED TAG", node.tag, full_node_text(node))
         raise BrokenException
 
     if not validate_children(node):
-        print("BROKEN TAG", node.tag, node_text(node))
+        print("BROKEN TAG", node.tag, full_node_text(node))
         raise BrokenException
 
     return True
