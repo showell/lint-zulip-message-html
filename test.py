c7b38897548dbd1485e5e025d1bb05d3321dd9bd
Steve Howell <showell30@yahoo.com>
2024-12-27 12:50:09 +0000

    ease debugging

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 import json
 from lxml import html
-from validator import validate
+from validator import validate, BrokenException
+
+def validate_html(s):
+    if s == "":
+        return True
+    ast = html.fromstring(s)
+    if not validate(ast):
+        print("BROKEN (full HTML follows):\n", s)
+        return False
+    return True
 
 json_payload = open("cases.json").read()
 for fixture in json.loads(json_payload)['regular_tests']:
     s = fixture["expected_output"]
-    if s == "":
-        continue
-    ast = html.fromstring(s)
-    if not validate(ast):
-        print("BROKEN BIG", s)
+    try:
+        success = validate_html(s)
+    except BrokenException:
+        print ("FAIL")
+        break
+    if not success:
+        print("FAIL")
         break
--- validator.py
+++ validator.py
@@ -1,16 +1,23 @@
 from rules import RULES
 from collections import defaultdict
+from lxml import etree
 
 RULES_DICT = defaultdict(list)
+
+class BrokenException(Exception):
+    pass
 
 for rule in RULES:
     RULES_DICT[rule["tag"]].append(rule)
 
+def node_text(node):
+    return etree.tostring(node, encoding="unicode", method="html")
+
 def validate_children(rule, node):
     children = node.getchildren()
     if children and not rule.get("children"):
-        print("UNEXPECTED CHILDREN", node.tag, node.text)
-        return False
+        print("UNEXPECTED CHILDREN", node.tag, node_text(node))
+        raise BrokenException
 
     for c in children:
         if not validate(c):
@@ -24,14 +31,12 @@ def validate_rule(rule, node):
 
 def validate(node):
     if node.tag not in RULES_DICT:
-        print("UNSUPPORTED TAG", node.tag, node.text)
-        return False
+        print("UNSUPPORTED TAG", node.tag, node_text(node))
+        raise BrokenException
 
     for rule in RULES_DICT[node.tag]:
         if validate_rule(rule, node):
             return True
 
-    print("BROKEN TAG", node.tag, node.text)
-    return False
-
-
+    print("BROKEN TAG", node.tag, node_text(node))
+    raise BrokenException
