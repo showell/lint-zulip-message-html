8e328a678ee3c76d9f8ffebaebb0bd252728af77
Steve Howell <showell30@yahoo.com>
2024-12-27 15:10:16 +0000

    simplify

--- rules.py
+++ rules.py
@@ -1,109 +1,59 @@
-"""
-We build a list of rules here to validate Zulip markdown
-HMTL.  Note that the caller puts these rules into a
-defaultdict of lists to make the checks more efficient,
-so the order here isn't actually relevant.
-"""
+ALL_TAGS = {
+    "a",
+    "annotation",
+    "blockquote",
+    "br",
+    "code",
+    "del",
+    "div",
+    "em",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "hr",
+    "img",
+    "li",
+    "math",
+    "mfrac",
+    "mi",
+    "mn",
+    "mo",
+    "mrow",
+    "msub",
+    "msubsup",
+    "msup",
+    "mtext",
+    "ol",
+    "p",
+    "pre",
+    "semantics",
+    "span",
+    "strong",
+    "table",
+    "tbody",
+    "td",
+    "th",
+    "thead",
+    "time",
+    "tr",
+    "ul",
+    "video",
+}
 
-PARENT_RULES = [
-    dict(tag="a"),
-    dict(tag="annotation"),
-    dict(tag="blockquote"),
-    dict(tag="code"),
-    dict(tag="del"),
-    dict(tag="div"),
-    dict(tag="em"),
-    dict(tag="p"),
-    dict(tag="pre"),
-    dict(tag="semantics"),
-    dict(tag="span"),
-    dict(tag="strong"),
-]
-
-CHILD_RULES = [
-    dict(tag="br"),
-    dict(tag="hr"),
-    dict(tag="img"),
-    dict(tag="time"),
-    dict(tag="video"),
-]
-
-HEADER_RULES = [
-    dict(tag="h1"),
-    dict(tag="h2"),
-    dict(tag="h3"),
-    dict(tag="h4"),
-    dict(tag="h5"),
-    dict(tag="h6"),
-]
-
-LIST_RULES = [
-    dict(tag="li"),
-    dict(tag="ol"),
-    dict(tag="ul"),
-]
-
-TABLE_RULES = [
-    dict(tag="table"),
-    dict(tag="tbody"),
-    dict(tag="td"),
-    dict(tag="th"),
-    dict(tag="thead"),
-    dict(tag="tr"),
-]
-
-MATH_PARENT_RULES = [
-    dict(tag="math"),
-    dict(tag="mfrac"),
-    dict(tag="mrow"),
-    dict(tag="msub"),
-    dict(tag="msubsup"),
-    dict(tag="msup"),
-]
-
-MATH_CHILD_RULES = [
-    dict(tag="mi"),
-    dict(tag="mn"),
-    dict(tag="mo"),
-    dict(tag="mtext"),
-]
-
-RULES = (
-    PARENT_RULES
-    + CHILD_RULES
-    + HEADER_RULES
-    + LIST_RULES
-    + TABLE_RULES
-    + MATH_PARENT_RULES
-    + MATH_CHILD_RULES
-)
-
-"""
-The following list mined from ~250,000 real-world Zulip messages,
-It's imperfect:
-    * it omits tags like "p" that can include almost any other tag
-    * it may be over-restrictive on other things
-"""
 RESTRICTED_TAGS = dict(
-    a={'code', 'video', 'img'},
-    code={'span'},
-    em={'code', 'a'},
-    math={'semantics'},
-    mfrac={'mrow', 'mi'},
-    mrow={'mo', 'mtext', 'mi', 'msubsup', 'mfrac', 'mn', 'msub', 'msup'},
-    msub={'mi'},
-    msubsup={'mo', 'mn', 'msup'},
-    msup={'mo', 'mn', 'mi'},
-    ol={'li'},
-    pre={'span', 'code'},
-    semantics={'mrow', 'annotation'},
-    span={'span', 'math'},
-    strong={'span', 'code', 'em', 'a'},
-    table={'thead', 'tbody'},
-    tbody={'tr'},
-    thead={'tr'},
-    tr={'th', 'td'},
-    ul={'li'},
+    a={"code", "video", "img"},
+    code={"span"},
+    ol={"li"},
+    pre={"span", "code"},
+    span={"span", "math"},
+    table={"thead", "tbody"},
+    tbody={"tr"},
+    thead={"tr"},
+    tr={"th", "td"},
+    ul={"li"},
 )
 
 LEAF_TAGS = {
--- validator.py
+++ validator.py
@@ -1,23 +1,15 @@
-from rules import RESTRICTED_TAGS, RULES, LEAF_TAGS
-from collections import defaultdict
+from rules import ALL_TAGS, RESTRICTED_TAGS, LEAF_TAGS
 from lxml import etree
-
-RULES_DICT = defaultdict(list)
-
 
 class BrokenException(Exception):
     pass
-
-
-for rule in RULES:
-    RULES_DICT[rule["tag"]].append(rule)
 
 
 def node_text(node):
     return etree.tostring(node, encoding="unicode", method="html")
 
 
-def validate_children(rule, node):
+def validate_children(node):
     children = node.getchildren()
 
     if children and node.tag in LEAF_TAGS:
@@ -27,7 +19,9 @@ def validate_children(rule, node):
     for c in children:
         if node.tag in RESTRICTED_TAGS:
             if c.tag not in RESTRICTED_TAGS[node.tag]:
-                print(f"UNEXPECTED CHILD OF {node.tag}", node.tag, node_text(node))
+                print(
+                    f"UNEXPECTED CHILD {c.tag} OF {node.tag}", node.tag, node_text(node)
+                )
                 raise BrokenException
 
         if not validate(c):
@@ -35,20 +29,13 @@ def validate_children(rule, node):
     return True
 
 
-def validate_rule(rule, node):
-    if not validate_children(rule, node):
-        return False
-    return True
-
-
 def validate(node):
-    if node.tag not in RULES_DICT:
+    if node.tag not in ALL_TAGS:
         print("UNSUPPORTED TAG", node.tag, node_text(node))
         raise BrokenException
 
-    for rule in RULES_DICT[node.tag]:
-        if validate_rule(rule, node):
-            return True
+    if not validate_children(node):
+        print("BROKEN TAG", node.tag, node_text(node))
+        raise BrokenException
 
-    print("BROKEN TAG", node.tag, node_text(node))
-    raise BrokenException
+    return True
