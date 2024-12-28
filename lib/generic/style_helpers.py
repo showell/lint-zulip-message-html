d8a14c982541c63c037829c06f74b01929fd5cf6
Steve Howell <showell30@yahoo.com>
2024-12-28 15:06:15 +0000

    style_helpers

---
+++ lib/generic/style_helpers.py
@@ -0,0 +1,19 @@
+def check_style(style, valid_keys):
+    """
+    Example format:
+        height:0.8889em;vertical-align:-0.1944em;
+
+    We don't actually validate the values, just the keys.
+
+    TODO: Add sanity checks for the actual values.
+    """
+    frags = [frag for frag in style.split(";") if frag != ""]
+
+    for frag in frags:
+        if ":" not in frag:
+            return False
+        css_key = frag.split(":")[0]
+        if css_key not in valid_keys:
+            return False
+
+    return True
--- lib/zulip/style_checkers.py
+++ lib/zulip/style_checkers.py
@@ -1,26 +1,6 @@
 from ..generic.debug_helpers import debug_info, BadZulipHtmlException
 from ..generic.lxml_helpers import full_node_text
-
-
-def check_style(style, valid_keys):
-    """
-    Example format:
-        height:0.8889em;vertical-align:-0.1944em;
-
-    We don't actually validate the values, just the keys.
-
-    TODO: Add sanity checks for the actual values.
-    """
-    frags = [frag for frag in style.split(";") if frag != ""]
-
-    for frag in frags:
-        if ":" not in frag:
-            return False
-        css_key = frag.split(":")[0]
-        if css_key not in valid_keys:
-            return False
-
-    return True
+from ..generic.style_helpers import check_style
 
 
 SPAN_VALID_KEYS = {
